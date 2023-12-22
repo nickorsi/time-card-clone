2bb2d31788d932f08a7380b46e6a09f4cb4695cd
Nicholas <nick.orsi1992@gmail.com>
2023-12-21 20:17:50 -0800

    Added code to create table schemas and seed the data base.

--- models.py
+++ models.py
@@ -12,3 +12,441 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Project(db.model):
+    """Projects in the system.
+    Relationships built to the following classes:
+    -StaffRole (join table)
+    -Staff
+    -CraftRole (join table)
+    -Craft
+    -CostCode
+    """
+
+    __tablename__ = "projects"
+
+    code = db.Column(
+        db.String(20),
+        primary_key=True,
+    )
+
+    project_name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    staff = db.relationship(
+        'Staff',
+        secondary="staff_roles",
+        backref="projects"
+    )
+
+    craft = db.relationship(
+        'Craft',
+        secondary="craft_roles",
+        backref="projects"
+    )
+
+    cost_codes = db.relationship(
+        'CostCode',
+        backref="project"
+    )
+
+class Staff(db.model):
+    """Staff members in the system.
+    Relationships built to the following classes:
+    -StaffRole (join table)
+    -Project
+    -DailyReport
+    """
+
+    __tablename__ = "staff"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    email = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True,
+    )
+
+    clearance = db.Column(
+        db.Integer,
+        db.ForeignKey('clearances.level'),
+        nullable=False,
+        default=1,
+    )
+
+    password = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    daily_reports = db.relationship('DailyReport', beackref='author')
+
+    def __repr__(self):
+        return f"<Staff #{self.id}: {self.first_name}, {self.last_name}, {self.email}, {self.clearance}>"
+
+    @classmethod
+    def register(cls, first_name, last_name, email, clearance, password):
+        """Register a staff person.
+
+        Hashes password and adds user to session.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        staff = Staff(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            clearance=clearance,
+            password=hashed_pwd,
+        )
+
+        db.session.add(staff)
+
+        return staff
+
+    @classmethod
+    def authenticate(cls, email, password):
+        """Find user with `email` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for staff whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching staff (or if password is wrong), returns
+        False.
+        """
+
+        staff = cls.query.filter_by(email=email).one_or_none()
+
+        if staff:
+            is_auth = bcrypt.check_password_hash(staff.password, password)
+            if is_auth:
+                return staff
+
+        return False
+
+class StaffRole(db.Model):
+    """Staff Roles in the System.
+    Acts as a join table for Staff and Project
+    """
+
+    __tablename__ = "staff_roles"
+
+    project_code = db.Column(
+        db.String(20),
+        db.ForeignKey('projects.id'),
+        primary_key=True,
+    )
+
+    staff_id = db.Column(
+        db.Integer,
+        db.ForeignKey('staff.id'),
+        primary_key=True,
+    )
+
+    staff_role = db.Column(
+        db.String(50),
+        nullable=False,
+        default="",
+    )
+
+    project = db.relationship('Project', backref="staff_roles")
+    staff = db.relationship('Staff', backref="staff_roles")
+
+class Craft(db.model):
+    """Craft members in the system.
+    Relationships built to the following classes:
+    -CraftRole (join table)
+    -Project
+    """
+
+    __tablename__ = "craft"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    @classmethod
+    def register(cls, first_name, last_name):
+        """Register a craft person.
+        """
+
+        craft = Craft(
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        db.session.add(craft)
+
+        return craft
+
+
+class CraftRole(db.Model):
+    """Craft Roles in the System.
+    Acts as a join table for Craft and Project
+    """
+
+    __tablename__ = "craft_roles"
+
+    project_code = db.Column(
+        db.String(20),
+        db.ForeignKey('projects.id'),
+        primary_key=True,
+    )
+
+    craft_id = db.Column(
+        db.Integer,
+        db.ForeignKey('craft.id'),
+        primary_key=True,
+    )
+
+    craft_role = db.Column(
+        db.String(50),
+        nullable=False,
+        default="",
+    )
+
+    project = db.relationship('Project', backref="craft_roles")
+    craft = db.relationship('Craft', backref="craft_roles")
+
+class CostCode(db.Model):
+    """Cost codes saved in the system.
+    Relationships built to the following classes:
+    -Project
+    -Clearance
+    -DailyReportItem (join table)
+    -DailyReport
+    -CostCodeSummary
+    """
+
+    __tablename__ = "cost_codes"
+
+    code = db.Column(
+        db.String(20),
+        primary_key=True,
+        autoincrement=False,
+    )
+
+    name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    project_code = db.Column(
+        db.String(20),
+        db.ForeignKey('projects.id')
+    )
+
+    clearance_level = db.Column(
+        db.Integer,
+        db.ForeignKey('clearances.level')
+    )
+
+    budgeted_qty = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    qty_units = db.Column(
+        db.String(10),
+        nullable=False,
+    )
+
+    budgeted_mhrs = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    budgeted_cost = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+class Clearance(db.Model):
+    """Clearance levels saved in the system.
+    Relationships built to the following classes:
+    -Staff
+    -CostCode
+    """
+
+    __tablename__ = "clearances"
+
+    level = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=False,
+    )
+
+    name = db.Column(
+        db.String(20),
+        nullable=False,
+    )
+
+class DailyReport(db.Model):
+    """Daily Reports in the system.
+    Relationships built to the following classes:
+    -Staff
+    -DailyReportItem
+    -CostCodeSummary
+    -CraftSummary
+    """
+
+    __tablename__ = "dailyreports"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    date = db.Column(
+        db.Date,
+        nullable=False,
+    )
+
+    author_id = db.Column(
+        db.Integer,
+        db.ForeignKey('staff.id'),
+        nullable=False,
+    )
+
+    approver_id = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    status = db.Column(
+        db.String(20),
+        nullable=False,
+    )
+
+    hrs = db.relationship(
+        'DailyReportItem',
+        backref="daily_report"
+    )
+
+    code_details = db.relationship(
+        'CostCodeSummary',
+        backref="daily_reports",
+    )
+
+    craft_notes = db.relationship(
+        'CraftSummary',
+        backref="daily_report",
+    )
+
+class DailyReportItem(db.Model):
+    """Daily Report Items in the system.
+    Relationships built with the following classes:
+    -DailyReport
+    """
+
+    __tablename__ = "daily_report_items"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    daily_report_id = db.Column(
+        db.Integer,
+        db.ForeignKey('daily_reports.id'),
+    )
+
+    craft_id = db.Column(
+        db.Integer,
+        db.ForeignKey('craft.id'),
+    )
+
+    cost_code = db.Column(
+        db.String(20),
+        db.ForeignKey('cost_codes.code'),
+    )
+
+    hrs_worked = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+class CostCodeSummary(db.model):
+    """Cost Code Summaries in the system.
+    Relationships built with the following classes:
+    -DailyReport
+    """
+
+    daily_report_id = db.Column(
+        db.Integer,
+        db.ForeignKey('daily_reports.id'),
+        primary_key=True,
+    )
+
+    cost_code = db.Column(
+        db.String(20),
+        db.ForeignKey('cost_codes.code'),
+        primary_key=True,
+    )
+
+    qty_installed = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    note = db.Column(
+        db.String(500),
+        nullable=False,
+    )
+
+class CraftSummary(db.model):
+    """Craft Summaries in the system.
+    Relationships built with the following classes:
+    -DailyReport
+    """
+
+    daily_report_id = db.Column(
+        db.Integer,
+        db.ForeignKey('daily_reports.id'),
+        primary_key=True,
+    )
+
+    craft_id = db.Column(
+        db.Integer,
+        db.ForeignKey('craft.id'),
+        primary_key=True,
+    )
+
+    note = db.Column(
+        db.String(500),
+        nullable=False,
+    )
+
--- seed.py
+++ seed.py
@@ -1,6 +1,253 @@
-from models import [Classes], db #---> For making instances of a class to make records in the table
+from models import Project, Staff, StaffRole, Craft, CraftRole, CostCode
+from models import DailyReport, DailyReportItem, CostCodeSummary, CraftSummary
+from models import Clearance, db
 from app import app
 
 # Create all tables
 db.drop_all()
 db.create_all()
+
+# Create project
+
+project = Project('100000', 'Gross Reservoir')
+
+db.session.add(project)
+db.session.commit
+
+# Create clearance levels
+
+clr1 = Clearance(1, 'Eng')
+
+db.session.add(clr1)
+db.session.commit()
+
+clr2 = Clearance(2, 'Sup')
+
+db.session.add(clr2)
+db.session.commit()
+
+clr3 = Clearance(3, 'Business')
+
+db.session.add(clr3)
+db.session.commit()
+
+clr4 = Clearance(4, 'Project Eng')
+
+db.session.add(clr4)
+db.session.commit()
+
+clr5 = Clearance(5, 'Admin')
+
+db.session.add(clr5)
+db.session.commit()
+
+# Create staff
+
+staff1 = Staff.register('Engineer', 'Test', 'engineer@gmail.com', 1, 'engineer')
+
+db.session.commit()
+
+staff2 = Staff.register(
+    'Superintendent',
+    'Test',
+    'superintendent@gmail.com',
+    2,
+    'superintendent'
+)
+
+db.session.commit()
+
+staff3 = Staff.register('Business', 'Test', 'business@gmail.com', 3, 'business')
+
+db.session.commit()
+
+staff4 = Staff.register(
+    'Projecteng',
+    'Test',
+    'projecteng@gmail.com',
+    4,
+    'projecteng'
+)
+
+db.session.commit()
+
+staff5 = Staff.register('Admin', 'Test', 'admin@gmail.com', 5, 'admin')
+
+db.session.commit()
+
+# Create Staff Roles
+
+staff1.staff_roles.append(StaffRole(project_id=project.code, role="Exc Engineer"))
+
+db.session.commit()
+
+staff2.staff_roles.append(StaffRole(project_id=project.code, role="Exc Superintendent"))
+
+db.session.commit()
+
+staff3.staff_roles.append(StaffRole(project_id=project.code, role="Business Controller"))
+
+db.session.commit()
+
+staff4.staff_roles.append(StaffRole(project_id=project.code, role="Project Engineer"))
+
+db.session.commit()
+
+staff5.staff_roles.append(StaffRole(project_id=project.code, role="Project Admin"))
+
+db.session.commit()
+
+# Create craft
+
+craft1 = Craft.register('Foreman', 'Test')
+
+db.session.commit()
+
+craft2 = Craft.register('Operator', 'Test')
+
+db.session.commit()
+
+craft3 = Craft.register('Laborer', 'Test')
+
+db.session.commit()
+
+# Create craft roles
+
+craft1.craft_roles.append(CraftRole(project_id=project.code, role="Exc Foreman"))
+
+db.session.commit()
+
+craft2.craft_roles.append(CraftRole(project_id=project.code, role="Exc Operator"))
+
+db.session.commit()
+
+craft3.craft_roles.append(CraftRole(project_id=project.code, role="Exc Laborer"))
+
+db.session.commit()
+
+## Create cost codes
+
+code1 = CostCode("1000", "Clear and Grub", project.code, 1, 22500, "SF", 20, 3600)
+
+db.session.add(code1)
+db.session.commit()
+
+code2 = CostCode("1001", "Excavate", project.code, 1, 8333, "CY", 42, 7560)
+
+db.session.add(code2)
+db.session.commit()
+
+code3 = CostCode("1002", "Haul/Dump Exc", project.code, 1, 8333, "CY", 420, 33600)
+
+db.session.add(code3)
+db.session.commit()
+
+# Create daily report
+
+daily1 = DailyReport("2023.12.21", staff1.id, staff2.id, "submitted")
+
+db.session.add(daily1)
+db.session.commit()
+
+# Create daily report items
+
+item1 = DailyReportItem(daily1.id, craft1.id, code1.code, 4)
+
+db.session.add(item1)
+db.session.commit()
+
+item2 = DailyReportItem(daily1.id, craft1.id, code2.code, 4)
+
+db.session.add(item2)
+db.session.commit()
+
+item3 = DailyReportItem(daily1.id, craft1.id, code3.code, 2)
+
+db.session.add(item3)
+db.session.commit()
+
+item4 = DailyReportItem(daily1.id, craft2.id, code1.code, 5)
+
+db.session.add(item4)
+db.session.commit()
+
+item5 = DailyReportItem(daily1.id, craft2.id, code2.code, 5)
+
+db.session.add(item5)
+db.session.commit()
+
+item6 = DailyReportItem(daily1.id, craft3.id, code1.code, 2)
+
+db.session.add(item6)
+db.session.commit()
+
+item7 = DailyReportItem(daily1.id, craft3.id, code2.code, 4)
+
+db.session.add(item7)
+db.session.commit()
+
+item8 = DailyReportItem(daily1.id, craft3.id, code3.code, 4)
+
+db.session.add(item8)
+db.session.commit()
+
+# Create cost code summaries
+
+code_summary1 = CostCodeSummary(
+    daily1.id,
+    code1.code,
+    22500,
+    "Started and finished grubbing site today."
+)
+
+db.session.add(code_summary1)
+db.session.commit()
+
+code_summary2 = CostCodeSummary(
+    daily1.id,
+    code2.code,
+    200,
+    "Started exc today, had to spend time setting up operation."
+)
+
+db.session.add(code_summary2)
+db.session.commit()
+
+code_summary3 = CostCodeSummary(
+    daily1.id,
+    code3.code,
+    200,
+    "Started haul today, only got a single round of trucks out."
+)
+
+db.session.add(code_summary3)
+db.session.commit()
+
+# Create craft summaries
+
+craft_summary1 = CraftSummary(
+    daily1.id,
+    craft1.id,
+    "No injuries, took all breaks and lunch."
+)
+
+db.session.add(craft_summary1)
+db.session.commit()
+
+craft_summary2 = CraftSummary(
+    daily1.id,
+    craft2.id,
+    "No injuries, took all breaks and lunch."
+)
+
+db.session.add(craft_summary2)
+db.session.commit()
+
+craft_summary3 = CraftSummary(
+    daily1.id,
+    craft3.id,
+    "No injuries, took all breaks and lunch."
+)
+
+db.session.add(craft_summary3)
+db.session.commit()
